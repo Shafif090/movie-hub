e9b03707f1a5fdce19b3cbb3c7360afc7a6dc556
SHAFIF FERDOUS RAHMAN <shafifferdous@gmail.com>
2025-04-26 19:09:23 +0600

    Implemented an optimized search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,84 @@
-import { useEffect, useState } from "react"
-import Search from "./components/Search.jsx"
-import Spinner from './components/Spinner.jsx'
+import { useEffect, useState } from "react";
+import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
+import { useDebounce } from "react-use";
 
-const API_BASE_URL =  'https://api.themoviedb.org/3';
+const API_BASE_URL = "https://api.themoviedb.org/3";
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`
-  }
-}
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
-  const [isLoading, setisLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [deboundSearchTerm, setDeboundSearchTerm] = useState("");
 
-  const fetchMovies = async () => {
-    setisLoading(true);
-    setErrorMessage('');
+  useDebounce(() => setDeboundSearchTerm(searchTerm), 500, [searchTerm]);
+
+  const fetchMovies = async (query = "") => {
+    setIsLoading(true);
+    setErrorMessage("");
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
 
-      if(!response.ok) {
-        throw new Error('Failed to fetch movies');
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
       }
 
       const data = await response.json();
 
-      if(data.response == 'False'){
-        setErrorMessage(data.error || 'Failed to fetch movies');
+      if (data.response == "False") {
+        setErrorMessage(data.error || "Failed to fetch movies");
         setMovieList([]);
         return;
       }
 
       setMovieList(data.results || []);
-
-
-
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
-      setErrorMessage('Error fetching movies. Please try again later.');
+      setErrorMessage("Error fetching movies. Please try again later.");
     } finally {
-      setisLoading(false);
+      setIsLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(deboundSearchTerm);
+  }, [deboundSearchTerm]);
 
   return (
     <main>
-
       <div className="pattern" />
 
       <div className="wrapper">
         <header>
           <img src="./hero.png" alt="Hero Banner" />
-          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
 
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
         <section className="all-movies">
           <h2 className="mt-[40px]">All Movies</h2>
-          
+
           {isLoading ? (
             <Spinner />
           ) : errorMessage ? (
@@ -81,16 +86,14 @@ const App = () => {
           ) : (
             <ul>
               {movieList.map((movie) => (
-                <MovieCard ley={movie.id} movie={movie}/>
+                <MovieCard ley={movie.id} movie={movie} />
               ))}
             </ul>
           )}
         </section>
       </div>
-
-
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
